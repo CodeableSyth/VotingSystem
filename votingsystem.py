797056106157135082700ff463f69ae263cdb957
CodeSyth aka CodeableSyth <shaun.ramsey.jr@gmail.com>
2021-09-29 21:08:09 -0400

    changed floats to ints for comparisons

--- votingsystem.py
+++ votingsystem.py
@@ -12,14 +12,14 @@ PlayerOne = str( input ("Player 1 is ") )
 PlayerTwo = str( input ("Player 2 is ") )
 PlayerThree = str( input ("Player 3 is ") )
 while True:
-    h = float( input ("has " + PlayerOne + " Used any skips? 1 = yes") )
-    s = float( input ("has " + PlayerTwo + " Used any skips? 1 = yes") )
-    c = float( input ("has " + PlayerThree + " Used any skips? 1 = yes") )
-    if h==1:
+    h = int( input ("has " + PlayerOne + " Used any skips? 1 = yes: ") )
+    s = int( input ("has " + PlayerTwo + " Used any skips? 1 = yes: ") )
+    c = int( input ("has " + PlayerThree + " Used any skips? 1 = yes: ") )
+    if h == 1:
         skiph = skiph - 1
-    if s==1:
+    if s == 1:
         skips = skips - 1
-    if c==1:
+    if c == 1:
         skipc = skipc - 1
     print (str(PlayerOne) + " has " + str(skiph) + " Skips")
     print (str(PlayerTwo) + " has " + str(skips) + " Skips")
